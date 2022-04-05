4cc2ea3fc40055c9d81f91390a8bccb23922d4ed
Fabian <62375683+sxge@users.noreply.github.com>
2022-04-05 12:41:49 -0700

    Add files via upload

---
+++ file.py
@@ -0,0 +1,10 @@
+# Was ist ein String?
+"ABC" # <--- Das ist ein String
+
+variable_name = "Hallo!" # Das ist eine variable
+print(variable_name) # Wir printen die Variable
+
+
+a = 10  # Variable "a"
+b = 5 # Variable "b"
+print(a + b) # Ergebnis der beiden Variablen a und b bei einer Addition
